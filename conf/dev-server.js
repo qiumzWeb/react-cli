078b37e29304c0b934c78b987a0f32ec7befc504
qiumingzhen.qmz <qiumingzhen.qmz@cainiao.com>
2026-01-04 18:26:07 +0800

    ddd

--- conf/dev-server.js
+++ conf/dev-server.js
@@ -22,9 +22,15 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
     }
 })
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
+// capture previous NODE_STATUS so we can detect a restart that happened
+// before the compilation changed it to "running"
+var previousNodeStatus = process.env.NODE_STATUS || ''
 // // force page reload when html-webpack-plugin template changes
 compiler.hooks.make.tap('compilation', function (compilation) {
     compilation.hooks.finishModules.tap('html-webpack-plugin-after-emit', function (modules) {
+        // capture the status before flipping to running so done hook can
+        // know if we came from a 'rebuild' state
+        previousNodeStatus = process.env.NODE_STATUS
         process.env.NODE_STATUS = "running";
         hotMiddleware.publish({ action: 'reload' })
     })
@@ -32,8 +38,24 @@ compiler.hooks.make.tap('compilation', function (compilation) {
 // print compiler finished info
 compiler.hooks.done.tap('AfterCompiler', function(stats) {
   setTimeout(() => {
-    if (process.env.NODE_STATUS === 'rebuild') {
-        hotMiddleware.publish({ action: 'reload' })
+    // If we were restarted (previousNodeStatus === 'rebuild') then the
+    // hot middleware client in the browser may not have reconnected yet to
+    // receive a single publish. Send the reload multiple times with a few
+    // retries to increase the chance the client receives it.
+    if (previousNodeStatus === 'rebuild' || process.env.NODE_STATUS === 'rebuild') {
+        var delays = [0, 500, 1500, 3000]
+        delays.forEach(function (d) {
+            setTimeout(function () {
+                try {
+                    hotMiddleware.publish({ action: 'reload' })
+                } catch (e) {
+                    // swallow any errors from publish and continue retries
+                    console.error('hotMiddleware.publish error:', e && e.message)
+                }
+            }, d)
+        })
+        // reset captured status so subsequent compilations behave normally
+        previousNodeStatus = ''
     }
     console.log('webpack \x1b[1mcompiled \x1B[32mfinished \x1B[37min',new Date().toLocaleString(),'\x1b[0m')
   }, 2000)
